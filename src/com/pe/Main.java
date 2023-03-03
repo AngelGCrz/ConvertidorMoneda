b20b9cea3bd46861a6dff4caa8deaacb421f2525
AngelGCrz <angelgcrz@gmail.com>
2023-03-02 22:26:46 -0500

    upload files conversorDivisas and Main

--- src/com/pe/Main.java
+++ src/com/pe/Main.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.pe;
 
 /**
  *
@@ -9,6 +10,6 @@
  */
 public class Main {
     public static void main(String[] args) {
-        
+        ConversorDivisas conversorDivisas = new ConversorDivisas();
     }
 }
